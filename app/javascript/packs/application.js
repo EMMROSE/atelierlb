2a81eb67e33beda5598f907462a2672fb6040e00
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-03-30 11:54:06 +0200

    javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -21,14 +22,18 @@ require("channels")
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
-
-// External imports
 import "bootstrap";
-
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+import { loadDynamicBannerText } from '../components/banner';
+import { scrolledNav } from '../components/navbar';
+import { formSportSessions } from '../components/form';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+  // Call your JS functions here
+  // [...]
+  //initFlatPickr();
+  scrolledNav(); // scroll effect of the navbar on Home Page
+  //stopCarousel(); // stop boostrap carousel (for preview) to autoplay
+  //initMapbox();
+  formSportSessions(); // change activity class when clicked on (on sport session form creation)
+  loadDynamicBannerText();
 });
